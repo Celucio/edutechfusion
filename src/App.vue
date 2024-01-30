b42dfe1b54951764e6e3a18a37f073473d467ac8
Carlos <celucio@espe.edu.ec>
2024-01-30 18:51:31 -0500

    Login v4

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
 
-  <RouterView></RouterView>
+  <LoginIndex></LoginIndex>
 </template>
 
 <script>
+import LoginIndex from '@/views/LoginIndex.vue'
 export default {
   name: 'App',
+  components: {
+    LoginIndex
+  }
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@400;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap');
-
 </style>
