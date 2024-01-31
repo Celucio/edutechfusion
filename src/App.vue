1824ec65f7c4d474a804b5e78b3199863302d877
Carlos <celucio@espe.edu.ec>
2024-01-30 19:04:11 -0500

    Ojala funcione

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
-
-  <LoginIndex></LoginIndex>
+  <div>
+    <RouterView/>
+  </div>
 </template>
 
-<script>
-import LoginIndex from '@/views/LoginIndex.vue'
-export default {
-  name: 'App',
-  components: {
-    LoginIndex
-  }
-}
-</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@400;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap');
+
+#app{
+  padding-top: 10px;
+}
 </style>
